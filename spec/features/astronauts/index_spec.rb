becfbf1eea58a42849c8170c0a8f1072bd298beb
KaraJH <karas.jones@yahoo.com>
2023-02-17 11:17:43 -0700

    test: Order missions by alphabetical order

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -5,7 +5,22 @@ RSpec.describe "Astronauts Index Page" do
     @astronaut_1 = Astronaut.create!(name: "Joe Fly", age: 32, job: "Pilot")
     @astronaut_2 = Astronaut.create!(name: "Tim Space", age: 30, job: "Navigator")
     @astronaut_3 = Astronaut.create!(name: "Bob", age: 40, job: "Mechanic")
+
+    @mission_1 = Mission.create!(title: "Moon", time_in_space: 120)
+    @mission_2 = Mission.create!(title: "Jupiter", time_in_space: 500) 
+    @mission_3 = Mission.create!(title: "Mars", time_in_space: 250)
+
+    @astronaut_mission_1 = AstronautMission.create!(astronaut_id: @astronaut_1.id, mission_id: @mission_1.id)
+    @astronaut_mission_2 = AstronautMission.create!(astronaut_id: @astronaut_1.id, mission_id: @mission_2.id)
+    @astronaut_mission_3 = AstronautMission.create!(astronaut_id: @astronaut_1.id, mission_id: @mission_3.id)
+    @astronaut_mission_4 = AstronautMission.create!(astronaut_id: @astronaut_2.id, mission_id: @mission_1.id)
+    @astronaut_mission_5 = AstronautMission.create!(astronaut_id: @astronaut_2.id, mission_id: @mission_2.id)
+    @astronaut_mission_6 = AstronautMission.create!(astronaut_id: @astronaut_3.id, mission_id: @mission_1.id)
+    @astronaut_mission_7 = AstronautMission.create!(astronaut_id: @astronaut_3.id, mission_id: @mission_2.id)
+    @astronaut_mission_8 = AstronautMission.create!(astronaut_id: @astronaut_3.id, mission_id: @mission_3.id)
   end
+
+
   describe "when visiting the astronauts index page" do 
     it 'has a list of astronauts with their attributes' do 
       visit "/astronauts"
@@ -27,11 +42,33 @@ RSpec.describe "Astronauts Index Page" do
         expect(page).to have_content(@astronaut_3.age)
         expect(page).to have_content(@astronaut_3.job)
       end
+    end
+
+    it 'lists the average age of all astronauts' do 
+      visit "/astronauts"
 
       within(".average_age") do 
         expect(page).to have_content("Average Age: 34")
       end
+    end
 
+    it 'has a list of space missions in alphabetical order for each astronaut' do 
+      visit "/astronauts"
+
+      within("##{@astronaut_1.id}") do 
+        expect(@mission_2.title).to appear_before(@mission_3.title)
+        expect(@mission_3.title).to appear_before(@mission_1.title)
+      end
+
+      within("##{@astronaut_2.id}") do 
+        expect(@mission_2.title).to appear_before(@mission_1.title)
+    
+      end
+
+      within("##{@astronaut_3.id}") do 
+        expect(@mission_2.title).to appear_before(@mission_3.title)
+        expect(@mission_3.title).to appear_before(@mission_1.title)
+      end
     end
   end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -5,6 +5,19 @@ describe Astronaut, type: :model do
     @astronaut_1 = Astronaut.create!(name: "Joe Fly", age: 32, job: "Pilot")
     @astronaut_2 = Astronaut.create!(name: "Tim Space", age: 30, job: "Navigator")
     @astronaut_3 = Astronaut.create!(name: "Bob", age: 40, job: "Mechanic")
+
+    @mission_1 = Mission.create!(title: "Moon", time_in_space: 120)
+    @mission_2 = Mission.create!(title: "Jupiter", time_in_space: 500) 
+    @mission_3 = Mission.create!(title: "Mars", time_in_space: 250)
+
+    @astronaut_mission_1 = AstronautMission.create!(astronaut_id: @astronaut_1.id, mission_id: @mission_1.id)
+    @astronaut_mission_2 = AstronautMission.create!(astronaut_id: @astronaut_1.id, mission_id: @mission_2.id)
+    @astronaut_mission_3 = AstronautMission.create!(astronaut_id: @astronaut_1.id, mission_id: @mission_3.id)
+    @astronaut_mission_4 = AstronautMission.create!(astronaut_id: @astronaut_2.id, mission_id: @mission_1.id)
+    @astronaut_mission_5 = AstronautMission.create!(astronaut_id: @astronaut_2.id, mission_id: @mission_2.id)
+    @astronaut_mission_6 = AstronautMission.create!(astronaut_id: @astronaut_3.id, mission_id: @mission_1.id)
+    @astronaut_mission_7 = AstronautMission.create!(astronaut_id: @astronaut_3.id, mission_id: @mission_2.id)
+    @astronaut_mission_8 = AstronautMission.create!(astronaut_id: @astronaut_3.id, mission_id: @mission_3.id)
   end
   describe 'Validations' do
     it { should validate_presence_of :name }
@@ -20,7 +33,13 @@ describe Astronaut, type: :model do
   describe "::average_age" do 
     it 'is the average age of all astronauts' do 
       expect(Astronaut.average_age).to eq(34)
+    end
+  end
 
+  describe '#order_missions_by_alphabetical' do 
+    it 'lists the astronauts missions in alphabetical order' do 
+      # expect(@astronaut_1.order_missions_by_alphabetical).to eq([@mission_2, @mission_3, @mission_1])
+      expect(@astronaut_1.order_missions_by_alphabetical).to eq(["Jupiter", "Mars", "Moon"])
     end
   end
 end
